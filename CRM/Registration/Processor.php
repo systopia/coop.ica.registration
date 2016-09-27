3b32ae9cc195dd347476a507d5f70627a25dede6
systopia <endres@systopia.de>
2016-09-27 17:06:32 +0200

    adjust line item label

--- CRM/Registration/Processor.php
+++ CRM/Registration/Processor.php
@@ -222,7 +222,7 @@ class CRM_Registration_Processor {
       'entity_table'      => 'civicrm_participant',
       'entity_id'         => $participant['participant_id'],
       'contribution_id'   => $contribution_id,
-      'label'             => $participant['participant_fee_level'],
+      'label'             => "{$participant['last_name']}, {$participant['first_name']} ({$participant['participant_fee_level']})",
       'qty'               => 1,
       'unit_price'        => $participant['participant_fee_amount'],
       'line_total'        => $participant['participant_fee_amount'],
